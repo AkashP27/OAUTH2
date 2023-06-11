31a3100fc32eac5d68c6428a382d487b2b70273a
AkashP27 <akashphayade27@gmail.com>
2023-06-11 12:19:55 +0530

    oauth

---
+++ src/main/java/com/springSecurity/controller/AccountController.java
@@ -0,0 +1,34 @@
+package com.springSecurity.controller;
+
+import com.springSecurity.model.Accounts;
+import com.springSecurity.model.Customer;
+import com.springSecurity.repository.AccountRepository;
+import com.springSecurity.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AccountController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private AccountRepository accountsRepository;
+
+    @GetMapping("/myAccount")
+    public Accounts getAccountDetails(@RequestParam String email) {
+        List<Customer> customers = customerRepository.findByEmail(email);
+        if (customers != null && !customers.isEmpty()) {
+            Accounts accounts = accountsRepository.findByCustomerId(customers.get(0).getId());
+            if (accounts != null) {
+                return accounts;
+            }
+        }
+        return null;
+    }
+}
